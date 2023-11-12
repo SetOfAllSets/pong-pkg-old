c0bd0a8ff679c1a10562537aebf65e2b8b3b1320
= <=>
2023-11-11 23:57:35 -0800

    fixed some bugs, cleaned up some code, and made errors stop the program

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -6,70 +5,127 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <bsd/string.h>
-#include "verbs.c"
 
 /*
-string size is 68 because, if config dir is $HOME/.config/pong-pkg and the username is 32 chars long, the 
+string size is 68 because, if config dir is $HOME/.config/pong-pkg and the username is 32 chars long, the
 longest allowed at time of writing, then 45 chars should be enough to store the config dir. 68 is
 chosen to allow longer config dir paths than $HOME/.config/pong-pkg in $XDG_CONFIG_HOME for users with a 32 char name.
 */
 
-char* configDir = "/etc/pong-pkg";
-char* repoDir = "/etc/pong-pkg/repo";
+int run = 1;
+int exitCode = 0;
+char *configDir = "/etc/pong-pkg";
+char *repoDir = "/etc/pong-pkg/repo";
 char usrConfigDir[68];
 char usrRepoDir[73];
 
-char* concat(char *s1, char *s2)
+int concat(char *s1, char *s2, char *outputString, size_t outputSize)
 {
-    char* result = s1;
-    strlcpy(result, s1, strlen(s1) + strlen(s2) + 1);
-    strlcat(result, s2, strlen(s1) + strlen(s2) + 1);
-    return result;
+    if (outputSize < strlen(s1) + strlen(s2) + 1)
+    {
+        fprintf(stderr, "Could not concatonate strings, size of buffer too small. %lu<%lu", outputSize, strlen(s1) + strlen(s2) + 1);
+        exitCode = -1;
+        run = 0;
+        return 1;
+    }
+    strlcpy(outputString, s1, strlen(s1) + strlen(s2) + 1);
+    strlcat(outputString, s2, strlen(s1) + strlen(s2) + 1);
+    return 0;
 }
 
-//check for config dir and contents, if incomplete, create
-int initConfigDir(char* dir) {
+// check for config dir and contents, if incomplete, create
+int initConfigDir(char *dir)
+{
+    char repoDirTmp[strlen(dir) + 6];
+    concat(dir, "/repo", repoDirTmp, sizeof(repoDirTmp));
     struct stat st;
-    int dirStatus = stat(dir,&st);
-    //error code 2 is ENOENT; directory does not exist
-    char* repoDir = concat(dir, "/repo");
-    if(dirStatus != 0) {
-        if(errno == 2) {
-            if(mkdir(dir, 0700) != 0) {
-                printf("Could not create config directory (%s). Error code was %i", dir, errno);
+    int dirStatus = stat(dir, &st);
+    if (dirStatus != 0)
+    {
+        // error code 2 is ENOENT; directory does not exist
+        if (errno == 2)
+        {
+            errno = 0;
+            if (mkdir(dir, 0700) != 0)
+            {
+                fprintf(stderr, "Could not create config directory (%s). Error was: %s\n", dir, strerror(errno));
+                exitCode = errno;
+                run = 0;
             }
-        } else {
-            printf("Error accessing config directory (%s). Error was not ENOENT. Error code was %i", dir, errno);
+        }
+        else
+        {
+            fprintf(stderr, "Error accessing config directory (%s). Error was not ENOENT. Error was: %s\n", dir, strerror(errno));
+            exitCode = errno;
+            run = 0;
         }
     }
-    dirStatus = stat(repoDir,&st);
-    if(dirStatus != 0) {
-        if(errno == 2) {
-            if(mkdir(repoDir, 0700) != 0) {
-                printf("Could not create config directory (%s). Error code was %i", repoDir, errno);
+    dirStatus = stat(repoDirTmp, &st);
+    if (dirStatus != 0)
+    {
+        if (errno == 2)
+        {
+            if (mkdir(repoDirTmp, 0700) != 0)
+            {
+                fprintf(stderr, "Could not create repo directory (%s). Error was: %s\n", repoDirTmp, strerror(errno));
+                exitCode = errno;
+                run = 0;
             }
-        } else {
-            printf("Error accessing repo directory (%s). Error was not ENOENT. Error code was %i", repoDir, errno);
+        }
+        else
+        {
+            fprintf(stderr, "Error accessing repo directory (%s). Error was not ENOENT. Error was: %s\n", repoDirTmp, strerror(errno));
+            exitCode = errno;
+            run = 0;
         }
     }
     return 0;
 }
 
-int main(int argc, char *argv[]) {
-    if (getenv("XDG_CONFIG_HOME") != NULL) {
-        strlcpy(usrConfigDir, concat(getenv("XDG_CONFIG_HOME"), "/pong-pkg"), strlen(usrConfigDir) + strlen(concat(getenv("XDG_CONFIG_HOME"), "/pong-pkg")) + 1);
-    } else {
-        strlcpy(usrConfigDir, concat(getenv("HOME"), "/.config/pong-pkg"), strlen(usrConfigDir) + strlen(concat(getenv("HOME"), "/.config/pong-pkg")) + 1);
-    }
-    strlcpy(usrRepoDir, concat(usrConfigDir, "/repo"), (strlen(concat(usrConfigDir, "/repo") + strlen(usrRepoDir) + 1)));
-    //check for config dir and contents, if incomplete, create
-    initConfigDir(usrConfigDir);
-    if (argc <= 1) {
-        printf("Not enough arguments");
-    } else {
-        if (strcmp(argv[1], "install") == 0 || strcmp(argv[1], "update") == 0 || strcmp(argv[1], "sync")) {
-            //run corresponding script (for sync that would be the update script for the repo package)
-        }
-    }
+// run script for verb
+int runScript(int argc, char *argv[])
+{
     return 0;
+}
+int main(int argc, char *argv[])
+{
+    while (run)
+    {
+        if (getenv("XDG_CONFIG_HOME") != NULL)
+        {
+            concat(getenv("XDG_CONFIG_HOME"), "/pong-pkg", usrConfigDir, sizeof(usrConfigDir));
+        }
+        else
+        {
+            concat(getenv("HOME"), "/.config/pong-pkg", usrConfigDir, sizeof(usrConfigDir));
+        }
+        concat(usrConfigDir, "/repo", usrRepoDir, sizeof(usrRepoDir));
+        // check for config dir and contents, if incomplete, create
+        initConfigDir(usrConfigDir);
+        if (argc <= 1)
+        {
+            fprintf(stderr, "Not enough arguments\n");
+            exitCode = -1;
+            run = 0;
+        }
+        else
+        {
+            if (strcmp(argv[1], "install") == 0 || strcmp(argv[1], "update") == 0 || strcmp(argv[1], "remove") == 0)
+            {
+                // run corresponding script (for sync that would be the update script for the repo package)
+                if (argc >= 3)
+                {
+                    // e
+                }
+                else
+                {
+                    fprintf(stderr, "Not enough arguments for verb %s\n", argv[1]);
+                    exitCode = -1;
+                    run = 0;
+                }
+            }
+        }
+        run = 0;
+    }
+    return exitCode;
 }
